23a86b2fa637b4133909fd8aa6eac28ee88156e1
spindycell <31498152+spindycell@users.noreply.github.com>
2018-01-08 17:30:55 +0700

    update responsive lagi mirror logo semnas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage({
     navigation: true,
-		navigationPosition: 'right',
-		scrollOverflow: true
+		navigationPosition: 'right'
   });
+});
+
+$(function(){ // this replaces document.ready
+	$(window).on("load", function(){
+		$('.se-pre-con').fadeOut('slow', function() {
+			$(this).remove();
+		});
+	});
 });
